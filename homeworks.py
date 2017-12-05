6dda8d6d15ed7e804a807752dff5edd0a39e1344
Vitalii87 <$>
2017-12-05 20:58:23 +0200

    add

---
+++ homeworks.py
@@ -0,0 +1,19 @@
+'''# 1 task
+
+H='1234 9u ytreee' 
+
+#print(H.isspace()) - not working
+print(len(H.split(' '))-1) #1.2  print(a1.isspace()) 
+print(len(H.split('.'))-1) #1.3
+a1='Homework'
+a2='homework'
+print(a1.center(100,' ')) #1.4
+print(a2.capitalize()) #1.5'''
+
+
+input1=input('enter year')
+if input1==str():
+ print('yes')
+
+h1='12324 ij n '
+print(h1.find(' '))
